92670738f7a316fb9c5e3ec9570146c9d93b36f4
Innoviox <sarras305@gmail.com>
2018-01-15 16:32:06 -0500

    Initial Testing

---
+++ src/main/java/jatch/JSONExample.java
@@ -0,0 +1,19 @@
+package main.java.jatch;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import main.java.com.cedarsoftware.util.io.JsonReader;
+
+public class JSONExample {
+	public static void main(String[] args) throws IOException {
+		String json = new String(Files.readAllBytes(Paths.get("project.json")), StandardCharsets.UTF_8);
+		Map<String, Object> arg = new HashMap<String, Object>();
+		arg.put(JsonReader.USE_MAPS, true);
+		Map<String, Object> data = (Map<String, Object>) JsonReader.jsonToJava(json, arg);
+	}
+}
---
+++ src/main/java/jatch/UnzipFile.java
@@ -0,0 +1,26 @@
+package main.java.jatch;
+
+import java.io.*;
+import java.util.zip.*;
+
+public class UnzipFile {
+    public static void main(String[] args) throws IOException {
+        String fileZip = "compressed.zip";
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while(zipEntry != null){
+            String fileName = zipEntry.getName();
+            File newFile = new File("unzipTest/" + fileName);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+}
